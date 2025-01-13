cc1fb45ceaab9264eaf0e08761212c100e830874
Sriram <Sriram@gmail.com>
2025-01-13 09:46:34 +0530

    Updated startminikube.sh with additional addons and configs for starting

--- kubernetes/startminikube.sh
+++ kubernetes/startminikube.sh
@@ -1,3 +1,9 @@
-# Adapt as necessary
+# Adapt the below as necessary
 minikube start --vm-driver=docker --container-runtime=containerd --memory 4400 --cpus 4
 minikube addons enable metrics-server
+
+# a more
+#minikube start --vm-driver=docker --container-runtime=containerd --mount-string="/home/demouser:/data" --mount  --memory 10000 --cpus 4
+#minikube addons enable metrics-server
+#minikube addons enable default-storageclass
+#minikube addons enable storage-provisioner
