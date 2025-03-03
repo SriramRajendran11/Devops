f8d2053d5694c7da7ef536ed44226244637ca224
Seshagiri Sriram <SeshagiriSriram@gmail.com>
2025-03-03 12:54:53 +0530

    Fixed rootless/root

--- install_rootless_docker.sh
+++ install_rootless_docker.sh
@@ -1,9 +1,11 @@
 sudo apt-get update && sudo apt-get install -y uidmap iptables dbus-user-session fuse-overlayfs
 #docker is already installed 
 if [ -f /usr/bin/docker ]; then 
+	docker rm -f $(docker ps -qa) 
+	docker rmi -f $(docker images -qa) 
 	sudo systemctl stop  --now docker.service docker.socket
 	sudo systemctl disable --now docker.service docker.socket
-	./remove_root_docker.sh
+	./remove_root_docker.sh 
 else
 	echo "Docker not installed moving on" 	
 	# install docker.. 
@@ -13,7 +15,10 @@ fi
 export FORCE_ROOTLESS_INSTALL=1
 # Note that this will remove docker binaries..
 curl -fsSL https://get.docker.com/rootless | sh 
-echo "export PATH=/home/${USER}/bin:\$PATH #ROOTLESS DOCKER"  >> $HOME/.bashrc
+c=$(grep -c '^.*#ROOTLESS' $HOME/.bashrc) 
+if [ ${c} -le 0 ]; then 
+	echo "export PATH=/home/${USER}/bin:\$PATH #ROOTLESS DOCKER"  >> $HOME/.bashrc
+fi 
 systemctl --user start docker
 systemctl --user enable  docker
 sudo loginctl enable-linger $(whoami)
